9164c76b4f6e010b7c8bddb558abe666bebd6bd2
IngTian <tzy20001018@gmail.com>
2021-10-04 17:05:27 -0400

    feat: completed the foundation

--- main.c
+++ main.c
@@ -57,15 +57,80 @@ struct UserCommand *getUserCommand(char *prompt) {
  * @param command A command.
  * @param childProcesses All of the child processes.
  */
-void executeBuiltInCommands(struct UserCommand *command, struct ChildProcessBlock* childProcesses[]) {
-    
+void executeBuiltInCommands(
+        struct UserCommand *command,
+        struct ChildProcessBlock *childProcesses[],
+        int numberOfChildProcesses
+) {
+    char *executionFileName = command->args[0];
+    if (strcmp(executionFileName, "pwd") == 0) {
+        // TODO Complete the pwd function.
+    } else if (strcmp(executionFileName, "cd") == 0) {
+        // TODO Complete the cd function.
+    } else if (strcmp(executionFileName, "exit") == 0) {
+        // TODO Complete the exit function.
+    } else if (strcmp(executionFileName, "fg") == 0) {
+        // TODO Complete the fg function.
+    } else if (strcmp(executionFileName, "jobs") == 0) {
+        // TODO Complete the jobs function.
+    }
+}
+
+int isBuiltInFunction(char *executionFileName) {
+    return strcmp(executionFileName, "pwd") == 0 ||
+           strcmp(executionFileName, "cd") == 0 ||
+           strcmp(executionFileName, "exit") == 0 ||
+           strcmp(executionFileName, "fg") == 0 ||
+           strcmp(executionFileName, "jobs") == 0;
+}
+
+/**
+ * Executing the user command in another process.
+ * @param userCommand A user command, stored in the heap.
+ */
+void executeUserCommands(struct UserCommand *userCommand) {
+    // TODO Execute a user command, take care of the piping and output redirection if needed.
 }
 
 int main(void) {
+
+    // This is a managing block for various child processes.
+    struct ChildProcessBlock *backgroundChildProcessBlocks[1000];
+    int numberOfChildProcesses = 0;
+
     while (1) {
         struct UserCommand *command = getUserCommand("\n>> ");
         printf("COMMAND RECEIVED: %s\nCOMMAND LENGTH: %d\nCOMMAND IN BACKGROUND: %d\n", *command->args,
                command->commandLength, command->isRunningInBackground);
+        /*
+         *  If the command is a built-in command,
+         *  we simply execute it in the main thread.
+         *  If not, we create a child process and
+         *  execute it.
+         */
+        if (isBuiltInFunction(command->args[0]))
+            executeBuiltInCommands(command, backgroundChildProcessBlocks, numberOfChildProcesses);
+        else {
+            int *childProcessStatus = 0, childProcess = fork();
+            if (childProcess == 0)
+                executeUserCommands(command);
+            else if (command->isRunningInBackground) {
+                // If the command is meant to run in the background,
+                // we shall not intervene with it except take a record
+                // of its pid and relevant information.
+                struct ChildProcessBlock *aNewBackgroundChildProcess = malloc(sizeof(struct ChildProcessBlock));
+                aNewBackgroundChildProcess->command = command;
+                aNewBackgroundChildProcess->pid = childProcess;
+                backgroundChildProcessBlocks[numberOfChildProcesses++] = aNewBackgroundChildProcess;
+            } else if (!command->isRunningInBackground)
+                // We shall wait for the child process to complete if
+                // it is not meant to run in the background.
+                waitpid(childProcess, childProcessStatus, 0);
+        }
+
+        // Since the previous command is no longer needed,
+        // we are going to destroy it.
+        free(command);
     }
     return 0;
 }
