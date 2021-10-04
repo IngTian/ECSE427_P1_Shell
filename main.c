85527b89e255610678eabec66104bbab34b5b5b5
IngTian <tzy20001018@gmail.com>
2021-10-04 16:07:23 -0400

    docs: added inline comments

--- main.c
+++ main.c
@@ -3,45 +3,69 @@
 #include <string.h>
 #include <stdlib.h>
 
-int getcmd(char *prompt, char *args[], int *background)
-{
-    int length, i = 0;
-    char *token, *loc;
+struct UserCommand {
+    char *args[20];
+    int isRunningInBackground;
+    int commandLength;
+};
+
+struct ChildProcessBlock {
+    int pid;
+    struct UserCommand *command;
+};
+
+/**
+ * Get user commands from stdin.
+ * @param prompt Define the prompt.
+ * @return User Command.
+ */
+struct UserCommand *getUserCommand(char *prompt) {
+
+    struct UserCommand *result = malloc(sizeof(struct UserCommand));
     char *line = NULL;
+
+    // Receive Input from user.
     size_t linecap = 0;
     printf("%s", prompt);
-    length = getline(&line, &linecap, stdin);
-    if (length <= 0)
-    {
-        exit(-1);
-    }
-    // Check if background is specified..
+    if (getline(&line, &linecap, stdin) <= 0) exit(-1);
+
+    // Check if background is specified.
+    char *loc;
     if ((loc = index(line, '&')) != NULL)
-    {
-        *background = 1;
-        *loc = ' ';
-    }
+        result->isRunningInBackground = 1;
     else
-        *background = 0;
-    while ((token = strsep(&line, " \t\n")) != NULL)
-    {
+        result->isRunningInBackground = 0;
+    if (loc != NULL) *loc = ' ';
+
+    // Parse user inputs.
+    char *token;
+    int commandLength = 0;
+    while ((token = strsep(&line, " \t\n")) != NULL) {
         for (int j = 0; j < strlen(token); j++)
             if (token[j] <= 32)
                 token[j] = '\0';
-            if (strlen(token) > 0)
-                args[i++] = token;
+        if (strlen(token) > 0)
+            result->args[commandLength++] = token;
     }
-    return i;
+    result->commandLength = commandLength;
+
+    return result;
 }
 
-int main(void)
-{
-    char *args[20];
-    int bg;
-    while (1)
-    {
-        bg = 0;
-        int cnt = getcmd("\n>> ", args, &bg);
-        printf("Received Command As %s\nIs Running In the Background: %d\n", *args, bg);
+/**
+ * Execute built in commands in the parent process.
+ * @param command A command.
+ * @param childProcesses All of the child processes.
+ */
+void executeBuiltInCommands(struct UserCommand *command, struct ChildProcessBlock* childProcesses[]) {
+    
+}
+
+int main(void) {
+    while (1) {
+        struct UserCommand *command = getUserCommand("\n>> ");
+        printf("COMMAND RECEIVED: %s\nCOMMAND LENGTH: %d\nCOMMAND IN BACKGROUND: %d\n", *command->args,
+               command->commandLength, command->isRunningInBackground);
     }
+    return 0;
 }
